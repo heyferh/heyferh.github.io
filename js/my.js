7c7d34437180e14bb1a4f00c7ed1e99eacac2cd8
ferh <hey.ferh@gmail.com>
2015-11-02 23:30:04 +0300

    fixes

--- js/my.js
+++ js/my.js
@@ -1,16 +1,19 @@
 function sendMessage() {
-	var name = $("#name").val();
-	var email = $("#email").val();
-	var messageText = $("#message").val();
-	$.ajax({
-	 type: 'POST',
-     url:"http://sandbox-heyferh.rhcloud.com/sandbox/message",
-     data: {"name":name, "email":email, "messageText":messageText},
-     success:function(json){
-     	Materialize.toast('Thank you for your message!', 4000)
-     },
-     error:function(){
-         
-     }      
-});
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var messageText = $("#message").val();
+    $.ajax({
+        type: 'POST',
+        url: "http://sandbox-heyferh.rhcloud.com/sandbox/message",
+        data: {"name": name, "email": email, "messageText": messageText},
+        success: function () {
+            $("#name").val('');
+            $("#email").val('');
+            $("#message").val('');
+            Materialize.toast('Thank you for your message!', 4000)
+        },
+        error: function () {
+
+        }
+    });
 }
